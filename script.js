2deea57ad8388f6c842214754ba0c39e2345d0e6
Fannandya Sutan <sfannandya@gmail.com>
2026-04-29 15:05:18 +0700

    menambahkan script js

---
+++ script.js
@@ -0,0 +1,70 @@
+let daftarAnggota = [
+  {
+    nama: "Fannandya sutan sakti pratama",
+    email: "fannandya.sutan.ft24@mail.umy.ac.id",
+    minat: "Web Development",
+  },
+  { nama: "Budi Santoso", email: "budi@gmail.com", minat: "Desain UI/UX" },
+];
+
+function tampilkanTabelAnggota() {
+  let tbody = document.getElementById("memberTableBody");
+  if (tbody) {
+    tbody.innerHTML = "";
+    for (let i = 0; i < daftarAnggota.length; i++) {
+      let anggota = daftarAnggota[i];
+      tbody.innerHTML += `
+                <tr>
+                    <td>${i + 1}</td>
+                    <td>${anggota.nama}</td>
+                    <td>${anggota.email}</td>
+                    <td>${anggota.minat}</td>
+                </tr>
+            `;
+    }
+  }
+}
+
+tampilkanTabelAnggota();
+
+function simpanData(event) {
+  event.preventDefault();
+
+  let inputNama = document.getElementById("inputNama").value;
+  let inputEmail = document.getElementById("inputEmail").value;
+  let inputMinat = document.getElementById("inputMinat").value;
+
+  daftarAnggota.push({
+    nama: inputNama,
+    email: inputEmail,
+    minat: inputMinat,
+  });
+
+  alert("Berhasil Mendaftar!\nNama: " + inputNama);
+
+  document.getElementById("tampilNama").innerText = inputNama;
+  document.getElementById("tampilEmail").innerText = inputEmail;
+  document.getElementById("tampilMinat").innerText = inputMinat;
+
+  document.getElementById("hasilForm").style.display = "block";
+
+  return false;
+}
+
+function tampilInfoGambar() {
+  alert("Ini adalah gambar kegiatan belajar anggota Tech Community.");
+}
+
+function putarAudio() {
+  let audio = document.getElementById("audioLatar");
+  if (audio) {
+    audio.play();
+  }
+}
+
+function pauseAudio() {
+  let audio = document.getElementById("audioLatar");
+  if (audio) {
+    audio.pause();
+  }
+}
